c318eac3f60080d41ccd64b60acc9ad463b5a6bd
vinayboggavarapu <vinayb950529@gmail.com>
2024-01-25 21:36:45 +0530

    chore : update loader svg stroke color

--- src/components/SendButton.tsx
+++ src/components/SendButton.tsx
@@ -54,6 +54,7 @@ export const Spinner = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
     class={'animate-spin -ml-1 mr-3 h-5 w-5 ' + props.class}
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
+    stroke="#6b7280"
     viewBox="0 0 24 24"
     data-testid="loading-spinner"
   >
