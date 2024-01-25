19e7fce44a158f6778398d59b01e6f12b766ca68
Abhay Mittal <iamabhaymittal@gmail.com>
2024-01-25 22:03:34 +0530

    Update links and styles in Badge, TextInput, and DeleteIcon components

--- src/components/icons/DeleteIcon.tsx
+++ src/components/icons/DeleteIcon.tsx
@@ -1,5 +1,4 @@
 import { JSX } from 'solid-js/jsx-runtime';
-const defaultButtonColor = '#3B81F6';
 export const DeleteIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -8,8 +7,8 @@ export const DeleteIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
     height="24"
     viewBox="0 0 24 24"
     stroke-width="2"
-    stroke={props.color ?? defaultButtonColor}
     fill="none"
+    style={{ stroke: props.color || "#6b7280" }}
     stroke-linecap="round"
     stroke-linejoin="round"
   >
